93b654642cf49f9716dfb842d6ef8ca8065212c5
Ken-Lauren Daganio <akosiken@gmail.com>
2018-03-31 11:37:50 +0800

    adds spec to meet that sweet sweet 100%

--- spec/requests/api/v1/ping_spec.rb
+++ spec/requests/api/v1/ping_spec.rb
@@ -14,6 +14,21 @@ describe '/api/v1/ping', type: :api do
       end
     end
 
+    context 'invalid jwt' do
+      before do
+        payload = { user_id: test_user.id }
+        token = Auth::JWTEncode.call(payload)
+        token = token.slice(1, token.length)
+
+        header 'Authorization', "Bearer #{token}"
+        get '/api/v1/ping'
+      end
+
+      it 'responds with 401 - unauthorized' do
+        expect(last_response.status).to eq(401)
+      end
+    end
+
     context 'authenticated user' do
       before do
         payload = { user_id: test_user.id }
